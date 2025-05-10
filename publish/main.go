ecf4a524968b56ef28b7450e98b964c6b2f2a61a
Péter Szarvas <contact@peterszarvas.hu>
2025-05-10 12:31:18 +0200

    feat: new publish script

--- publish/main.go
+++ publish/main.go
@@ -11,28 +11,42 @@ import (
 )
 
 func main() {
-	tag, err := utils.GetVersion()
+	// checking version
+	version, err := utils.GetVersion()
 	if err != nil {
 		fmt.Printf("Can not get version: %s", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Printf("Publishing version: %s\n", tag)
+	fmt.Printf("Publishing version: %s\n", version)
 
-	ok, err := utils.RemoteTagExists(tag)
+	// checking tag
+	ok, err := utils.RemoteTagExists(version)
 	if err != nil {
-		fmt.Printf("Error checking tag %s: %s\n", tag, err.Error())
+		fmt.Printf("Error checking tag %s: %s\n", version, err.Error())
 	}
 	if !ok {
-		fmt.Printf("Tag does not exists yet: %s \n", tag)
+		fmt.Printf("Tag does not exists yet: %s \n", version)
 	} else {
-		fmt.Printf("Tag already exists: %s \n", tag)
+		fmt.Printf("Tag already exists: %s \n", version)
 		os.Exit(1)
 	}
 
+	// changing go.mod files for templates:
+	// - backup to go.mod.bkp
+	// - replacing goat version
+	// - removing "replace" directives
 	subfolders, err := utils.GetSubfolders("templates")
 	for _, folder := range subfolders {
 		modFilePath := filepath.Join(folder, "go.mod")
+		backupModFilePath := filepath.Join(folder, "go.mod.bkp")
+
+		err := utils.CopyFile(modFilePath, backupModFilePath)
+		if err != nil {
+			fmt.Printf("Can not backup modfile: %s\n", folder)
+			os.Exit(1)
+		}
+
 		modFile, err := os.Open(modFilePath)
 		if err != nil {
 			fmt.Printf("No modfile found in: %s\n", folder)
@@ -49,7 +63,7 @@ func main() {
 				if start != -1 {
 					parts := strings.Fields(line[start:])
 					if len(parts) == 2 {
-						line = line[:start] + parts[0] + " " + tag
+						line = line[:start] + parts[0] + " " + version
 					}
 				}
 			}
@@ -72,82 +86,55 @@ func main() {
 			os.Exit(1)
 		}
 
-		fmt.Printf("Version replaced for folder %s to %s\n", folder, tag)
+		fmt.Printf("Version replaced for folder %s to %s\n", folder, version)
 	}
 
-	// TODO: git add .
-	// TODO: git commit -m "publish: version"
-	// TODO: git push
-	// TODO: git push --tags
-	// TODO: restore original go.mod files
+	// git stuff
+	err = utils.Cmd("git", "add", ".")
+	if err != nil {
+		fmt.Printf("Error with \"git add .\": %s\n", err.Error())
+		os.Exit(1)
+	}
 
-	///////////// OLD CODE: /////////////
+	err = utils.Cmd("git", "commit", "-m", fmt.Sprintf("publish: %s", version))
+	if err != nil {
+		fmt.Printf("Error with \"git commit\": %s\n", err.Error())
+		os.Exit(1)
+	}
 
-	// Parse the folder flag
-	// folderFlag := flag.String("folder", "", "Folder to publish (e.g., cli/xy)")
-	// flag.Parse()
-	//
-	// if *folderFlag == "" {
-	// 	fmt.Println("Please specify a folder using --folder")
-	// 	os.Exit(1)
-	// }
+	err = utils.Cmd("git", "push")
+	if err != nil {
+		fmt.Printf("Error with \"git push\": %s\n", err.Error())
+		os.Exit(1)
+	}
 
-	//
-	// folder := *folderFlag
-	// fp := filepath.Join(strings.Split(folder, "/")...)
-	// goMod := filepath.Join(fp, "go.mod")
-	// localGoMod := filepath.Join(fp, "go.mod.local")
-	//
-	// // Backup go.mod to go.mod.local
-	// if err := utils.CopyFile(goMod, localGoMod); err != nil {
-	// 	fmt.Printf("Error backing up go.mod: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Remove replace lines from go.mod
-	// if err := utils.RemoveReplaceLines(goMod); err != nil {
-	// 	fmt.Printf("Error removing replace lines: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Commit the changes
-	// if err := utils.Cmd("git", "add", "."); err != nil {
-	// 	fmt.Printf("Error running git add: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	// if err := utils.Cmd("git", "commit", "--amend"); err != nil {
-	// 	fmt.Printf("Error running git commit --amend: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Tag the new version
-	// tag := fmt.Sprintf("%s%s", folder, version)
-	// if err := utils.Cmd("git", "tag", tag, "-m", tag); err != nil {
-	// 	fmt.Printf("Error creating git tag: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Push the changes and tags
-	// if err := utils.Cmd("git", "push"); err != nil {
-	// 	fmt.Printf("Error pushing to git: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	// if err := utils.Cmd("git", "push", "--tags"); err != nil {
-	// 	fmt.Printf("Error pushing tags to git: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Restore the original go.mod
-	// if err := utils.CopyFile(localGoMod, goMod); err != nil {
-	// 	fmt.Printf("Error restoring go.mod: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// // Remove the backup
-	// if err := os.Remove(localGoMod); err != nil {
-	// 	fmt.Printf("Error removing go.mod.local: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Printf("Version %s published successfully\n", version)
+	err = utils.Cmd("git", "push", "--tags")
+	if err != nil {
+		fmt.Printf("Error with \"git push --tags\": %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// restore modfiles
+	for _, folder := range subfolders {
+		modFilePath := filepath.Join(folder, "go.mod")
+		backupModFilePath := filepath.Join(folder, "go.mod.bkp")
+
+		err = os.Remove(modFilePath)
+		if err != nil {
+			fmt.Printf("Can not remove modfile: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		err = utils.CopyFile(backupModFilePath, modFilePath)
+		if err != nil {
+			fmt.Printf("Can not restore modfile: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		err = utils.Cmd("go", "mod", "tidy")
+		if err != nil {
+			fmt.Printf("Error with \"git push --tags\": %s\n", err.Error())
+			os.Exit(1)
+		}
+	}
 }
