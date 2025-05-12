d50be08a27a54e42832398a3778b1ed4778c1330
Péter Szarvas <contact@peterszarvas.hu>
2025-05-12 10:13:32 +0200

    cleanup

--- pkg/utils/version.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/peterszarvas94/goat/pkg/version"
-
-func GetVersion() (string, error) {
-	return version.Version, nil
-}
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,3 +1,3 @@
 package version
 
-var Version = "v0.3.13"
+var Version = "v0.3.14"
